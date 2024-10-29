72569063aa78d803fa9cb913139b9ddfb58baa24
Gavin Sommers <115888788+Gavis420@users.noreply.github.com>
2024-10-29 18:47:50 +0000

    Start listing ideas, create basic hello world function

---
+++ euler1.cpp
@@ -0,0 +1,21 @@
+/*
+* If we list all the natural numbers below 10 that are multiples
+* of 3 or 5, we get 3, 5, 6, and 9. The sum of these multiples is 23. 
+* Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
+//ideas for solution:
+// use loop for multiplication method. meaning:
+// multiply 3 x i until it is >= 1000 and do the same with 5 x i until >= 1000. 
+    // as you go either continually add the number to a growing total, or 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Prints hello world
+    cout << "Hello World";
+
+    return 0;
+}
