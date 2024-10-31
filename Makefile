6426cc9a5ccbac05356902166da01a4123fb840b
Gavin Sommers <115888788+Gavis420@users.noreply.github.com>
2024-10-31 16:56:59 +0000

    Change ints to long long to not overflow over one million bound

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 euler1: euler1.cpp
-	g++ -o euler1 euler1.cpp
+	g++ -std=c++2a -o euler1 euler1.cpp
 
 run: euler1
 	./euler1
--- euler1.cpp
+++ euler1.cpp
@@ -6,47 +6,52 @@
 */
 
 #include <iostream>
-using namespace std;
 
-int sumn(int n, int d) {
+
+long long int sumn(long long int n, long long int d) {
     n /= d;
     return d * n * (n + 1) / 2;
 }
 
-void functionA(int &a)  // Pass total by reference
+void functionA(long long int L, long long int &a)  // Pass total by reference
 {
-    for(int i = 5; i < 1000; i += 5)
-    {
-        if(i % 3 != 0)
-        {
+    for (int i = 5; i < L; i += 5) {
+        if (i % 3 != 0) {
             a += i;
         }
     }
-    
-    for(int i = 3; i < 1000; i += 3)
-    {
+
+    for (int i = 3; i < L; i += 3) {
         a += i;
     }
 }
 
-void functionB(int &b)  // Pass total by reference
+void functionB(long long int L, long long int &b)  // Pass total by reference
 {
-    for(int i = 0; i < 1000; i++)
-    {
-        if(i%3 == 0 || i%5 == 0)
-        {
+    for (long long int i = 0; i < L; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
             b += i;
         }
     }
 }
 
-int main()
-{
-    int total = 0;
+void functionC(long long int L, long long int &s) {
+    int a = 3, b = 5;
+    s = sumn(L - 1, a) + sumn(L - 1, b) - sumn(L - 1, a * b);
+}
 
-    functionA(total);
+int main() {
+    long long int bound = 0;
+    long long int total = 0;
 
-    cout << "\nThe sum of multiples of 3 or 5 up to 1000 is " << total << ".\n\n";
-    
+    std::cout << "Enter an upper bound? " << std::endl;
+    std::cin >> bound;
+
+    functionA(bound, total);
+    functionB(bound, total);
+    functionC(bound, total);
+
+    std::cout << "\nThe sum of multiples of 3 or 5 up to " << bound << " is " << total << ".\n\n";
+
     return 0;
 }
