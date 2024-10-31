906fb81eb15254f006da7add825f3cf93801d3cf
Gavin Sommers <115888788+Gavis420@users.noreply.github.com>
2024-10-31 17:11:39 +0000

    Add function descriptions

--- euler1.cpp
+++ euler1.cpp
@@ -7,13 +7,19 @@
 
 #include <iostream>
 
-
+// The sumn function calculates the sum of all natural numbers less than or equal
+// to a given number n that are divisible by another number d. It does so by using
+// the formula for the sum of the first k natural numbers, where k is the largest
+// integer less than or equal to nn divided by d.
 long long int sumn(long long int n, long long int d) {
     n /= d;
     return d * n * (n + 1) / 2;
 }
 
-void functionA(long long int L, long long int &a)  // Pass total by reference
+// Function A uses two statements and modulus to iterate by 3 or 5 up to
+// the given bound. Numbers divisible by 3 or 5 are added to the total.
+// Numbers that are divisible by both are only counted once.
+void functionA(long long int L, long long int &a)  
 {
     for (int i = 5; i < L; i += 5) {
         if (i % 3 != 0) {
@@ -26,7 +32,10 @@ void functionA(long long int L, long long int &a)  // Pass total by reference
     }
 }
 
-void functionB(long long int L, long long int &b)  // Pass total by reference
+// Function B is similar to function A. This one uses the for loop to iterate through
+// every number up to the bound instead of just by 3's and 5's. This uses modulus  
+// to check for either and will add if either or both pass.
+void functionB(long long int L, long long int &b)
 {
     for (long long int i = 0; i < L; i++) {
         if (i % 3 == 0 || i % 5 == 0) {
@@ -35,11 +44,14 @@ void functionB(long long int L, long long int &b)  // Pass total by reference
     }
 }
 
+// Function C uses a completely different approach designed from math formulas.
+// This uses the sumn function with Gauss's summation formula.
 void functionC(long long int L, long long int &s) {
     int a = 3, b = 5;
     s = sumn(L - 1, a) + sumn(L - 1, b) - sumn(L - 1, a * b);
 }
 
+// Main Function
 int main() {
     long long int bound = 0;
     long long int total = 0;
@@ -47,8 +59,7 @@ int main() {
     std::cout << "Enter an upper bound? " << std::endl;
     std::cin >> bound;
 
-    functionA(bound, total);
-    functionB(bound, total);
+    // using functionC as it should scale the best with larger bounds
     functionC(bound, total);
 
     std::cout << "\nThe sum of multiples of 3 or 5 up to " << bound << " is " << total << ".\n\n";
